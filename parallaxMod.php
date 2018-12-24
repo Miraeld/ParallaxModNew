980f8a915ba2be02954641970dd6bc3b53157eb6
Miraeld <gael@luxury-concept.com>
2018-12-24 18:32:19 +0700

    Upload of images working

--- parallaxMod.php
+++ parallaxMod.php
@@ -228,6 +228,46 @@ class ParallaxMod extends Module
           }
 
           if (!empty($allFields['PARALLAXMOD_IMAGE']) && !empty($allFields['PARALLAXMOD_TITLE'])) {
+            $target_dir = _PS_UPLOAD_DIR_;
+    				$target_file = $target_dir . basename($_FILES['PARALLAXMOD_IMAGE']["name"]);
+    				$uploadOk = 1;
+    				$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+            ///
+            $check = getimagesize($_FILES['PARALLAXMOD_IMAGE']["tmp_name"]);
+  					if($check !== false) {
+  						echo "File is an image - " . $check["mime"] . ".";
+  						$uploadOk = 1;
+  					} else {
+  						echo "File is not an image.";
+  						$uploadOk = 0;
+  					}
+            if (file_exists($target_file)) {
+    					echo "Sorry, file already exists.";
+    					$uploadOk = 0;
+    				}
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+      				&& $imageFileType != "gif" ) {
+      					echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      					$uploadOk = 0;
+      				}
+      				// Check if $uploadOk is set to 0 by an error
+      				if ($uploadOk == 0) {
+      					echo "Sorry, your file was not uploaded.";
+      				}
+      				else
+      				{
+      					if (move_uploaded_file($_FILES['PARALLAXMOD_IMAGE']["tmp_name"], $target_file))
+      					{
+      						echo "The file ". basename($_FILES['PARALLAXMOD_IMAGE']["name"]). " has been uploaded.";
+      						$file_location = basename($_FILES['PARALLAXMOD_IMAGE']["name"]);
+                  $allFields['PARALLAXMOD_IMAGE'] = $target_file;
+      					}
+      					else
+      					{
+      						echo "Sorry, there was an error uploading your file.";
+      					}
+    				}
+
             if ($this->insertDb($allFields)) {
               ?>
                 <script>
